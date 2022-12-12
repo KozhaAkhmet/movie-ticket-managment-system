c2d84ffc848a2c341e51e57c7d9e1f56be2ee3a6
KozhaAkhmet <kozha.akhmt@gmail.com>
2022-12-12 20:36:04 +0300

    Last update

--- lib/Movie.py
+++ lib/Movie.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-
+from lib import People
 
 class Show:
     def __init__(self, id, played_at, movie, start_time, end_time):
@@ -12,13 +12,20 @@ class Show:
 
 
 class Movie:
-    def __init__(self, title, description, duration_in_mins, language, release_date, country, genre, added_by):
+    def __init__(self, title: str,
+                 description: str,
+                 duration_in_mins: int,
+                 language: str, release_date: datetime,
+                 city: str,
+                 genre: str,
+                 added_by: People.Admin):
+
         self.__title = title
         self.__description = description
         self.__duration_in_mins = duration_in_mins
         self.__language = language
         self.__release_date = release_date
-        self.__country = country
+        self.__city = city
         self.__genre = genre
         self.__movie_added_by = added_by
 
